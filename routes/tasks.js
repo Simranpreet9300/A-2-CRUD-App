0fab39c1ce49fc57c22dc921d03facf34164c238
Simranpreet Kaur <simarkaur3320@gmail.com>
2020-12-06 06:10:10 -0500

    Added Update and Delete operations

--- routes/tasks.js
+++ routes/tasks.js
@@ -50,5 +50,73 @@ router.post('/add', (req, res, next) => {
     })
 })
 
+//GET tasks/delete/ - colon in the path represents a URL parameter
+router.get('/delete/:_id', (req, res, next) => {
+    //store the selected id in a local variable
+    var _id = req.params._id;
+    //Use Mongoose to delete the selected document from the DB
+    Task.remove({ _id: _id }, (err) => {
+        if (err) {
+            console.log(err)
+            res.end(err)
+        }
+        else {
+            res.redirect('/tasks')
+        }
+    })
+})
+
+///////   GET    //////////
+///////tasks/edit/....  populate edit for  with my existing task values
+router.get('/edit/:_id', (req, res, next) => {
+    //store the _id parameter in a local var
+    var _id = req.params._id
+    //use the selected _id to lookup the matching document
+    Task.findById(_id, (err, task) => {
+        if (err) {
+            console.log(err)
+            res.end(err)
+        }
+        else {
+            res.render('tasks/edit',
+                {
+                    task: task
+                })
+        }
+    })
+})
+
+
+/////// POST ///////tasks/edit/:_id -> updated selected task document
+router.post('/edit/:_id', (req, res, next) => {
+    var _id = req.params._id
+    //parse checkbox to a boolean
+    let available = false
+    if (req.body.available === "on") {
+        available = true
+    }
+
+    console.log('Availability value: ' + req.body.available)
+    //instantiate a Task object with the new values from the form submission
+    var task = new Task({
+        _id: _id,
+        name: req.body.name,
+        specialties: req.body.specialties,
+        gender: req.body.gender,
+        priority: req.body.priority,
+        available: available
+    })
+    //update document with selected id, passing new task object to replace old values
+    Task.update({ _id: _id }, task, (err) => {
+        if (err) {
+            console.log(err)
+            res.end(err)
+        }
+        else {
+            res.redirect('/tasks')
+        }
+    })
+})
+
 //exposes this file as Public
 module.exports = router;
